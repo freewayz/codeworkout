87bfc7533c7f3ed620ed0ddba3bf8f8b93d17362
rocket <fr33wayz@gmail.com>
2016-04-23 16:49:00 +0100

    added test for payment

--- paystack_app/apps/app1/tests.py
+++ paystack_app/apps/app1/tests.py
@@ -15,6 +15,9 @@ class ApiTestCase(TestCase):
         token = self.paypalApi.test_paypal_api_access_token()
         self.assertIsNotNone(token, "Access token is not gotten")
 
+    def test_that_paypal_payment_recv_ok_status(self):
+        status = self.paypalApi.test_paypal_payment_with_info()
+        self.assertEqual(status, 200, "Status is not ok")
 
 
 
